cabdeebc7c749e7775ae43068b53db42ef3522e8
Kartavya Thapa <hennybeerseeker@gmail.com>
2023-09-28 23:10:27 +0530

    field required all

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -47,10 +47,10 @@ const Form=()=>{
     return(
         <div>
             <form className="text-black font-bold grid gap-2">
-                <input type="text" name='fname'  placeholder="First Name" maxLength={30} className=" w-[25em] focus:outline-none p-2 placeholder:italic placeholder:text-center rounded-lg"/>
-                <input type="text" name='lname'  placeholder="Second Name" maxLength={30} className=" w-[25em] focus:outline-none p-2 placeholder:italic placeholder:text-center rounded-lg"/>
-                <input type="email" name='email'  placeholder="Email" maxLength={30} className=" w-[25em] focus:outline-none p-2 placeholder:italic placeholder:text-center rounded-lg"/>
-                <textarea name="message"  id="message" cols={30} rows={10} placeholder="Message" className=" w-[25em] focus:outline-none p-2 placeholder:italic placeholder:text-center rounded-lg"/>
+                <input type="text" name='fname' required  placeholder="First Name" maxLength={30} className=" w-[25em] focus:outline-none p-2 placeholder:italic placeholder:text-center rounded-lg"/>
+                <input type="text" name='lname' required  placeholder="Second Name" maxLength={30} className=" w-[25em] focus:outline-none p-2 placeholder:italic placeholder:text-center rounded-lg"/>
+                <input type="email" name='email' required placeholder="Email" maxLength={30} className=" w-[25em] focus:outline-none p-2 placeholder:italic placeholder:text-center rounded-lg"/>
+                <textarea name="message"  id="message" required cols={30} rows={10} placeholder="Message" className=" w-[25em] focus:outline-none p-2 placeholder:italic placeholder:text-center rounded-lg"/>
                 <button type="submit" onClick={handleButton} className="bg-red-500 hover:bg-red-400 ease-linear duration-150 text-black p-2 rounded-full">Submit</button>
             </form>
         </div>
