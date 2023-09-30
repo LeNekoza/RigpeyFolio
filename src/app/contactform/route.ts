6ded942c4d971756b3ddad7dc24ea135e4d06463
Kartavya Thapa <hennybeerseeker@gmail.com>
2023-09-30 18:26:27 +0530

    supabase integration

--- src/app/contactform/route.ts
+++ src/app/contactform/route.ts
@@ -1,17 +1,30 @@
 "use strict";
-
 import { NextRequest, NextResponse } from "next/server";
+import supabase from '../utils/supabase'
 export async function POST(req:Request,res:NextResponse) {
     try{
-    const data = await req.json()
-    const {fname, lname, email, message} = data
+    const reqObj:formtype = await req.json()
+    const {fname, lname, email, message} = reqObj
     console.log(`New message from ${fname} ${lname}\nemail: ${email} \nMessage: ${message}`)
-   
-    
-    return NextResponse.json({status: 'success'})
-}
-catch(err){
+    type formtype = {
+        fname: string,
+        lname: string,
+        email: string,
+        message: string
+    }
+  const { data, error } = await supabase
+            .from('rigform')
+            .insert([
+            { fname: fname, lname:lname, email:email, message:message },
+            ])
+            .select()
+            return NextResponse.json({status: 'success'})
+       
+        }
+
+    catch(err){
     console.log(err)
     return NextResponse.json({status: 'error'})
-}
+    }
+
 }
---
+++ src/app/rigsec10101010/page.tsx
@@ -0,0 +1,48 @@
+"use client"
+import Footer from "../components/footer";
+import Nav from "../components/nav";
+import supabase from "../utils/supabase";
+import { useState } from "react";
+export default function RigSec(){
+    type Msg = {
+        fname: string,
+        lname: string,
+        email: string,
+        message: string,
+    }
+    const [msg, setMsg] = useState<Msg[]>([])
+    const fetchMsg = async () => {
+        const {data, error} = await supabase
+        .from('rigform')
+        .select('*')
+        if(error) return console.log(error)
+        setMsg(data||[])
+    }
+    fetchMsg()
+
+    return(
+        <div>
+            <Nav/>
+            <h1 className="text-[2.5em] font-extrabold text-center my-10 bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-yellow-500 select-none">LOGS</h1>
+            <div className="w-full h-[0.2em] bg-gradient-to-r from-red-500 to-yellow-500"></div>
+            <div className="p-5 font-bold ">Total Messages: {msg.length}</div>
+            <div className="flex justify-center">
+                <ul>
+                    {
+                        msg.map((msg, index) => (
+                            <li key={index} className="my-5">
+                                <div className="flex flex-col">
+                                    <span className="text-xl font-bold">{`${index + 1}.  `} {msg.fname} {msg.lname}</span>
+                                    <span className="text-lg">{`Email: ${msg.email}`}</span>
+                                    <span className="text-lg">{`Message: ${msg.message}`}</span>
+                                </div>
+                                <div className="w-full h-[2px] bg-gradient-to-r from-red-500 to-yellow-500 mb-20"></div>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+            <Footer/>
+        </div>
+    )
+}
